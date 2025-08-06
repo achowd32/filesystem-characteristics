7251b1b69e426cab4836d961998e33639b4b92e9
Atharv Chowdhary <atharv_chowdhary@brown.edu>
2025-08-06 15:15:37 -0400

    first commit with database file

---
+++ README.md
@@ -0,0 +1,2 @@
+# Filesystem Characteristics
+A set of scripts to help collate important characteristics of a given filesystem. Currently a work in progress.
---
+++ database.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# create database
+rm fsdb.db
+touch fsdb.db
+sqlite3 fsdb.db 'CREATE TABLE fsgraph(identifier int, name text, parent text);'
+
+# read from incoming stream
+id=0
+while IFS= read filename; do
+    parent="$(dirname "$filename")"
+    sqlite3 fsdb.db \
+        -cmd '.parameter init' \
+        -cmd ".parameter set :id $id" \
+        -cmd ".parameter set :filename '$filename'" \
+        -cmd ".parameter set :parent '$parent'" \
+        "INSERT INTO fsgraph values(:id, :filename, :parent);"
+    ((id++))
+done < <(cd "$1"; find . -exec echo {} \;)
